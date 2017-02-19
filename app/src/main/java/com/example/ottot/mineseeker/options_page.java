cbd2bc8ce216b1e0e2b7c97eefc609d49ad60ecf
otto <ohu@sfu.ca>
2017-02-18 23:22:06 -0800

    model done! all pages are set up and linked. help page is done a bit fix

--- app/src/main/java/com/example/ottot/mineseeker/options_page.java
+++ app/src/main/java/com/example/ottot/mineseeker/options_page.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class options_page extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class options_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_page);
+
+        Button done = (Button) findViewById(R.id.opt_ret_btn);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public static Intent makeIntent(Context userMenu) {
